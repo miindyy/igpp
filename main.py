3d3235a1e2647d680c552e48f680ac68d88d7486
geraldanthony26 <141295419+geraldanthony26@users.noreply.github.com>
2023-08-10 17:25:25 +0800

    main.py

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-
-import overheads 
+import overheads
 import cashonhand 
-import csv 
-from pathlib import Path 
- 
-csv_file_path = "overheadt.csv" 
-coh_csv_file_path = "coh day 0-90.csv" 
- 
-# Read data from "coh day 0-90.csv" using the cashonhand module 
-with open(coh_csv_file_path, newline="", encoding="UTF-8") as csvfile: 
-    # Create a csv reader object 
-    reader = csv.reader(csvfile) 
-    # Skip header 
-    next(reader) 
-     
-    # Call the cashonhand.coh() function with the reader object 
-    cashonhand.coh(reader) 
- 
-# Read data from "overheadt.csv" using the overheads module 
-with open(csv_file_path, newline="") as csvfile: 
-    # Create a csv reader object 
-    reader = csv.reader(csvfile) 
-    # Skip header 
-    next(reader) 
-     
-    # Call the overheads.overheads() function with the reader object and unpack the result 
-    HAHA, LOL = overheads.overheads(reader)
+import pnl
+import csv
+from pathlib import Path
 
->>>>>>> 8f000336d36696b5ee936d8f4ec8f57fca8a3a45
+oh_csv_file_path = Path.cwd() / "OVERHEADS.csv"
+coh_csv_file_path = Path.cwd() / "COH.csv"
+pnl_csv_file_path = Path.cwd() / "PNL.csv"
+
+# Read data from "OVERHEADS.csv" using the overheads module
+with oh_csv_file_path.open(mode="r", encoding="UTF-8", newline="") as csvfile: 
+    reader = csv.reader(csvfile) 
+    a = overheads.overheads(reader)
+        
+
+# Read data from "COH.csv" using the cashonhand module 
+with coh_csv_file_path.open(mode="r", encoding="UTF-8", newline="") as csvfile: 
+    reader = csv.reader(csvfile) 
+    b = cashonhand.coh(reader)  # Extract the desired value from the tuple
+
+# Read data from "PNL.csv" using the pnl module 
+with pnl_csv_file_path.open(mode="r", encoding="UTF-8", newline="") as csvfile: 
+    reader = csv.reader(csvfile) 
+    c = pnl.pnl(reader)  # Extract the desired value from the tuple
+
+# Write data to a text file
+with open("summary.txt", "w") as file:
+# Write data to a text file
+    file.write("Highest Overhead:\n")
+    file.write(f"[HIGHEST OVERHEAD]: {a[0].upper()}: {a[1]}%\n")
+    
+    file.write("Cash Deficit:\n")
+    for c_deficit in b[2]: # third item in b is list
+        file.write(f"{c_deficit}\n")
+    
+    file.write("Profit Deficit:\n")
+    for p_deficit in c[2]: # third item in c is list
+        file.write(f"{p_deficit}\n")
+    
